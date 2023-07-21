bc2de42fe425a5bdf9ece7893d376f34dd9aa207
Squigernog <44275774+Squigernog@users.noreply.github.com>
2023-07-21 10:11:29 -0700

    Soy command resize fix
    Soy command now resizes images of all sizes to fit into the speech bubble

--- cogs/textcommands.py
+++ cogs/textcommands.py
@@ -113,12 +113,14 @@ class textcommands(commands.Cog):
                 draw = ImageDraw.Draw(img)
                 
                 # Check for attachment
-                if(msg.attachments != None and (msg.attachments[0].content_type in ('image/jpeg', 'image/jpg', 'image/png'))):\
+                if(msg.attachments != None and (msg.attachments[0].content_type in ('image/jpeg', 'image/jpg', 'image/png', 'image/webp', 'image/gif'))):
                     # Resize and add to image
                     ref_img = msg.attachments[0]
                     with Image.open(requests.get(ref_img.url, stream=True).raw) as im:
-                        width, height = im.size
-                        ref_img_resize = im.resize((int(width/3), int(height/3)))
+                        ref_img_resize = resizeImage(im, speechBubble.width, speechBubble.height)
+                        """w = int(speechBubble.width * 0.7)
+                        h = int(speechBubble.height * 0.7)
+                        ref_img_resize = im.resize([w, h])"""
                         
                         # Calculate image offset
                         x_offset = ref_img_resize.width / 2
@@ -146,10 +148,39 @@ class textcommands(commands.Cog):
             await ctx.send("Invalid selection")
 
 def concatImages(im1, im2):
+    """
+    Vertically concatenate two images. im1 on top and im2 on bottom.
+    """
     dst = Image.new('RGB', (im1.width, im1.height + im2.height))
     dst.paste(im1, (0, 0))
     dst.paste(im2, (0, im1.height))
     return dst
+
+def resizeImage(img:Image.Image, speech_w:int, speech_h:int):
+    """
+    Resize image with relation to the speech bubble's size
+    If image is square, resize to 70% size of the speech bubble
+    If width or height is larger that the other measurement, resize larger measurement to 70%
+      of speech bubble size, then find the aspect ratio of image to set up a ratio equation to 
+      find the other value.
+    Ex.: if w < h, then x/(speech_h * 0.7) = W/H, where x is the resized width and W and H are
+      the original values of the image
+    """
+    ref_img = img
+    if (ref_img.width == ref_img.height):
+        w = int(speech_w * 0.7)
+        h = int(speech_h * 0.7)
+        ref_img = ref_img.resize([w, h])
+    elif (ref_img.width < ref_img.height):
+        h = int(speech_h * 0.7)
+        w = int(h * (ref_img.width / ref_img.height))
+        ref_img = ref_img.resize([w, h])
+    else:
+        w = int(speech_w * 0.7)
+        h = int(w * (ref_img.height / ref_img.width))
+        ref_img = ref_img.resize([w, h])
+
+    return ref_img
 
 async def setup(client:commands.Bot) -> None:
     await client.add_cog(textcommands(client))
